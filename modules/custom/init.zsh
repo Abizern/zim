0c5a54e0e594edfe9ad91640aec91bcf747707cf
Abizer Nasir <abizern@abizern.org>
2016-11-24 13:47:35 +0000

    Add emacs shortcuts to custom
    Signed-off-by: Abizer Nasir <abizern@abizern.org>

--- modules/custom/init.zsh
+++ modules/custom/init.zsh
@@ -5,4 +5,24 @@
 # any custom stuff should go here.
 # ensure that 'custom' exists in the zmodules array in your .zimrc
 
+#
+# Editors
+#
+alias ed='emacs --daemon'
+
+# These require emacs server to be running
+alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
+alias em='emacsclient -n'
+alias et='emacsclient -t'
+alias ec='emacsclient -nc'
+alias ek="emacsclient -e '(kill-emacs)'"
+alias er="ek;ed;ec" # Kill and restart an Emacs window
+
+# Try and fool emacs into letting me use this shell
+[[ $EMACS = t ]] && unsetopt zle
+
+export EDITOR='emacsclient -nc'
+export VISUAL='emacsclient -n'
+export ALTERNATE_EDITOR=''
+
 
