8ba0173f4fff12f87ca0a72872bfd4c1d2488f1a
Abizer Nasir <abizern@abizern.org>
2017-01-25 03:49:30 +0000

    Bring over some aliases

--- modules/custom/init.zsh
+++ modules/custom/init.zsh
@@ -43,4 +43,13 @@ eval $(thefuck --alias)
 #
 eval "$(rbenv init -)"
 
-    
+#
+# git
+#
+alias gdt='git difftool'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+#
+# Xcode
+#
+alias kill-dd='rm -rf ~/Library/Developer/Xcode/DerivedData'
